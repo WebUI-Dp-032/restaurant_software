a82e4bba5a974089d30fe65e4bca65406cf1468e
dimiork <dimiork@gmail.com>
2013-04-16 02:57:48 +0300

    gather all MenuModule data in Main user_controller.rb

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,6 +4,11 @@ class UserController < ApplicationController
   before_filter :authenticate_user!
   def index
     @role =  'main'
+    
+    #  need to preload MenuModule data
+    @groups = Group.all
+    @categories = Category.all;
+    @items = Item.all
   end
 
 end
