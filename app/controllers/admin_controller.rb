489a5ff111c9ecb0f6ca7ed998b7f2199fce1f2e
dimiork <dimiork@gmail.com>
2013-04-17 11:16:13 +0300

    backend admin controller adding data of MenuModule

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,6 +4,11 @@ class AdminController < ApplicationController
   before_filter :authenticate_admin!
   def index
     @role =  'admin'
+
+    #  need to preload MenuModule data
+    @groups = Group.all
+    @categories = Category.all;
+    @items = Item.all
   end
 
 end
